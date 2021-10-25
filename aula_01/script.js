1ac331767b0a42d9ba24316a9c030cc754f1d129
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2021-10-25 08:15:53 -0300

    Curso de JavaScript

---
+++ aula_01/script.js
@@ -0,0 +1,19 @@
+// Declarar uma variável com seu nome
+let nome = "Lucas";
+
+// Declarar uma variável com sua idade
+let idade = 22;
+
+// Declarar uma variável com a sua comida favorita e não atribuir valor
+var pizza;
+
+// Atribuir valor a sua comida favorita
+pizza = "bacon"
+
+// Declarar 5 variáveis diferentes sem valores
+let num1;
+let num2;
+let num3;
+let num4;
+let num5;
+
