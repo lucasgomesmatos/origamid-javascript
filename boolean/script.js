0b298458e99e6b466242df15a39471bd0d8cf62e
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2021-10-26 14:43:18 -0300

    numeros_operados e boolean

---
+++ boolean/script.js
@@ -0,0 +1,24 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20; // 35
+console.log(total);
+
+// Crie duas expressões que retornem NaN
+var expressao1 = '100s' - 5;
+var expressao2 = '100s' / 10;
+
+console.log(expressao1, expressao2);
+
+// Somar a string '200' com o número 50 e retornar 250
+var soma = +'200' + 50;
+console.log(soma)
+
+// Incremente o número 5 e retorne o seu valor incrementado
+var num = 5;
+console.log(++num)
+
+// Como dividir o peso por 2?
+var numero = '80' / 2;
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+var pesoPorDois = peso; // NaN (Not a Number)
+console.log(pesoPorDois)
