49fb3724ebc9226a1e3556d8c99a6b33e3dc5893
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2021-10-31 15:20:34 -0300

    Atribuição e Ternário

---
+++ atribuicao_ternario/script-aula.js
@@ -0,0 +1,16 @@
+var x = 5;
+var y = 10;
+x += y; // x = x + y (15)
+x -= y; // x = x - y (-5)
+x *= y; // x = x * y (50)
+x /= y; // x = x / y (0.5)
+x %= y; // x = x % y (0)
+x **= y; // x = x ** y (9765625)
+
+
+var idade = 19;
+var podeDirigir = (idade >= 18) ? 'Pode dirigir' : 'Não pode dirigir';
+console.log(podeDirigir) // Pode beber
+
+// condição ? true : false
+
