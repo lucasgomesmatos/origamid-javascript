79858f1e03d829bcb0125cb405b7a0e3306f5c2d
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-02-07 22:27:07 -0300

    Arrays e Lops

--- prototype/script.js
+++
@@ -1,9 +0,0 @@
-const pais = 'Brasil';
-
-const cidade = new String('Rio');
-
-const listaAnimais = ['cachorro', 'gato', 'cavalo']
-
-const lista = document.querySelectorAll('li');
-
-const listaArray = Array.prototype.slice.call(lista);
