a1a35190ae1a7cdd3c7654613f9587ff01004792
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-02-25 22:49:55 -0300

    hosting em javascript

--- javascript-para-iniciante/escopo/script-aula.js
+++ javascript-para-iniciante/escopo/script-aula.js
@@ -1,57 +1,144 @@
-// Servem para explicar o código
+// ESCOPO DE FUNÇÃO
 
-// Comentário de uma linha
+// Variáveis declaradas dentro de funções não são acessadas fora das mesmas.
 
-/*
-Comentário
-com diversas
-linhas
-*/
+function mostrarCarro() {
+  var carro = 'Fusca';
+  console.log(carro);
+}
 
-// var nome = 'Lucas';
+mostrarCarro(); // Fusca no console
+console.log(carro); // Erro, carro is not defined
 
-
-/* Comentar uma linha de código desativa a mesma. Não deixe linhas de código comentadas no arquivo final. */
-
-
-// OPERADORES DE ATRIBUIÇÃO
-
-// Podem funcionar como formas abreviadas
-
-var x = 5;
-var y = 10;
-x += y; // x = x + y (15)
-x -= y; // x = x - y (-5)
-x *= y; // x = x * y (50)
-x /= y; // x = x / y (0.5)
-x %= y; // x = x % y (0)
-x **= y; // x = x ** y (9765625)
+// Escopo evita o conflito entre nomes.
 
 
 
-// OPERADOR TERNÁRIO
-// Abreviação de condicionais com if e else
+// VARIÁVEL GLOBAL (ERRO)
+// Declarar variáveis sem a palavra chave var, const ou let, cria uma variável que pode ser acessar em qualquer escopo (global). Isso é um erro.
 
-var idade = 19;
-var podeBeber = (idade >= 18) ? 'Pode beber' : 'Não pode beber';
-console.log(podeBeber) // Pode beber
+function mostrarCarro() {
+  carro = 'Fusca';
+  console.log(carro);
+}
 
-// condição ? true : false
-
-// Geralmente utilizado quando precisamos atribuir um valor para uma variável, dependendo de uma condição
-
+mostrarCarro(); // Fusca
+console.log(carro); // Fusca
 
 
-// IF ABREVIADO
-// Não é necessário abrir e fechar as chaves {} quando retornamos apenas uma linha de código
+// 'use strict' impede isso.
 
-var possuiFaculdade = true;
-if(possuiFaculdade) console.log('Possui faculdade');
-else console.log('Não possui faculdade');
 
-// ou
-if(possuiFaculdade)
-  console.log('Possui faculdade');
-else
-  console.log('Não possui faculdade');
+// ESCOPO DE FUNÇÃO (PAI)
+// Variáveis declaradas no escopo pai da função, conseguem ser acessadas pelas funções.
 
+var carro = 'Fusca';
+
+function mostrarCarro() {
+  var frase = `Meu carro é um ${carro}`;
+  console.log(frase);
+}
+
+mostrarCarro(); // Meu carro é um Fusca
+console.log(carro);  // Fusca
+
+
+
+// ESCOPO DE BLOCO
+
+// Variáveis criadas com var, vazam o bloco. Por isso com a introdução do ES6 a melhor forma de declarmos uma variável é utilizando const e let, pois estas respeitam o escopo de bloco.
+
+if(true) {
+  var carro = 'Fusca';
+  console.log(carro);
+}
+console.log(carro); // Carro
+
+
+
+// VAR VAZA O BLOCO
+
+// Mesmo com a condição falsa, a variável ainda será declarada utilizando hoisting e o valor ficará como undefined.
+
+if(false) {
+  var carro = 'Fusca';
+  console.log(carro);
+}
+console.log(carro); // undefined
+
+
+// CONST E LET NO LUGAR DE VAR
+
+// A partir de agora vamos utilizar apenas const e let para declarmos variáveis.
+
+if(true) {
+  const carro = 'Fusca';
+  console.log(carro);
+}
+console.log(carro); // erro, carro is not defined
+
+
+
+// {} CRIA UM BLOCO
+
+// Chaves {} criam um escopo de bloco, não confundir com a criação de objetos = {}
+{
+  var carro = 'Fusca';
+  const ano = 2018;
+}
+console.log(carro); // Carro
+console.log(ano); // erro ano is not defined
+
+
+
+// FOR LOOP
+
+// Ao utilizar var dentro de um forloop, que é um bloco, o valor do variável utilizada irá vazar e existir fora do loop.
+
+for(var i = 0; i < 10; i++) {
+  console.log(`Número ${i}`);
+}
+console.log(i); // 10
+
+
+// FOR LOOP COM LET
+// Com o let evitamos que o número vaze.
+
+for(let i = 0; i < 10; i++) {
+  console.log(`Número ${i}`);
+}
+console.log(i); // i is not defined
+
+// CONST
+
+// Mantém o escopo no bloco, impede a redeclaração e impede a modificação do valor da variável, evitando bugs no código.
+
+const mes = 'Dezembro';
+mes = 'Janeiro'; // erro, tentou modificar o valor
+// const semana; // erro, declarou sem valor
+
+const data = {
+  dia: 28,
+  mes: 'Dezembro',
+  ano: 2018,
+}
+
+data.dia = 29; // Funciona
+data = 'Janeiro'; // erro
+
+// Variáveis com valores constantes devem utilizar o const.
+
+
+
+// LET
+
+// Mantém o escopo no bloco, impede a redeclaração, mas permite a modificação do valor da variável.
+
+let ano;
+ano = 2018;
+ano++;
+console.log(ano); // 2019
+
+// let ano = 2020; // erro, redeclarou a variável
+
+// Geralmente vamos utilizar o const.
--- javascript-para-iniciante/escopo/script.js
+++ javascript-para-iniciante/escopo/script.js
@@ -1,10 +1,30 @@
-function mostrarCarro() {
-  const carro = 'Fusca';
-  console.log(carro);
+// Por qual motivo o código abaixo retorna com erros?
+{
+  var cor = 'preto';
+  const marca = 'Fiat';
+  let portas = 4;
+  console.log(cor, marca, portas); 
+}
+// Porque é uma palavra chave do javascript
+
+// Como corrigir o erro abaixo?
+const dois = 2;
+
+function somarDois(x) {
+  return x + dois;
+}
+function dividirDois(x) {
+  return x / dois;
+}
+somarDois(4);
+dividirDois(6);
+
+// O que fazer para total retornar 500?
+var numero = 50;
+
+for(let numero = 0; numero < 10; numero++) {
+  console.log(numero);
 }
 
-mostrarCarro(); // fusca no console
-console.log(carro); // Erro, carro is not defined
-
-
-console.log()
+const total = 10 * numero;
+console.log(total);
