fcf98350b95b1c1763a22058f7b6c8b3bff68658
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-03-10 22:14:44 -0300

    dom

--- dom/o-que-e-dom/script.js
+++ dom/o-que-e-dom/script.js
@@ -1,1 +1,23 @@
-console.log(window.location.href)
+// Retorne o url da página atual utilizando o objeto window
+
+const url = window.location.href
+
+console.log(url)
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+
+const ativo = document.querySelector('.ativo');
+
+// Retorne a linguagem do navegador
+const linguagem = window.navigator.language
+
+console.log(linguagem)
+
+// Retorne a largura da janela 
+
+const largura = window.innerWidth
+
+console.log(largura)
+
+
