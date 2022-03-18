6f7c6e8560f798d765eaf8a9617cd8c7e65f781f
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-03-17 23:13:58 -0300

    Seleção em Js

--- dom/selecionar-o-dom/assets/scripts/script.js
+++ dom/selecionar-o-dom/assets/scripts/script.js
@@ -1,3 +1,21 @@
-const animais = document.getElementById('animais');
+// Retorne no console todas as imagens do site
+const imagens = document.querySelectorAll('img');
+console.log(imagens)
 
-console.log(animais.innerText)
+// Retorne no console apenas as imagens que começaram com a palavra imagem
+
+const imgImagens = document.querySelectorAll("[src^='./assets/img/imagem']");
+console.log(imgImagens)
+
+// Selecione todos os links internos (onde o href começa com #)
+const linksInternos = document.querySelectorAll("[href^='#']");
+console.log(linksInternos)
+
+// Selecione o primeiro h2 dentro de .animais-descricao
+const primeiroh2 = document.querySelector('.animais-descricao h2');
+console.log(primeiroh2)
+
+// Selecione o último p do site
+const ultimoParagrafo = document.querySelectorAll('p:last-child');
+console.log(ultimoParagrafo[ultimoParagrafo.length - 1])
+
