bb5d99976d1c43a1adb918e56a76ae59556b6d83
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-04-05 15:26:56 -0300

    eventos

--- dom/eventos/assets/scripts/script.js
+++ dom/eventos/assets/scripts/script.js
@@ -21,27 +21,27 @@ linkInternos.forEach(link => {
 // Selecione todos os elementos do site começando a partir do body,
 // ao clique mostre exatamente quais elementos estão sendo clicados
 
-// const elements = document.querySelectorAll('body *')
+const elements = document.querySelectorAll('body *')
 
-// const handleElement = (e) => {
-//   console.log(e.currentTarget)
-// }
+const handleElement = (e) => {
+  console.log(e.currentTarget)
+}
 
-// elements.forEach((element) => {
-//   element.addEventListener('click', handleElement);
-// })
+elements.forEach((element) => {
+  element.addEventListener('click', handleElement);
+})
 
 
 // Utilizando o código anterior, ao invés de mostrar no console,
 // remova o elemento que está sendo clicado, o método remove() remove um elemento
 
-// const handleRemove = (e) => {
-//   e.currentTarget.remove()
-// }
+const handleRemove = (e) => {
+  e.currentTarget.remove()
+}
 
-// elements.forEach((element) => {
-//   element.addEventListener('click', handleRemove);
-// })
+elements.forEach((element) => {
+  element.addEventListener('click', handleRemove);
+})
 
 // Se o usuário clicar na tecla (t), aumente todo o texto do site. 
 const windowClickEnlarge = (e) => {
