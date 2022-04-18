e48f4f049ec23d4747e07a6c462553c60acb94bf
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-04-17 23:10:22 -0300

    Animacao ao scrol

--- dom/animacao-ao-scroll/assets/scripts/script.js
+++ dom/animacao-ao-scroll/assets/scripts/script.js
@@ -61,10 +61,28 @@ const initScrollSuave = () => {
   );
 };
 
+const animaScroll = () => {
+  const sections = document.querySelectorAll(".js-scroll");
+  if (sections.length) {
+    const windowMetado = window.innerHeight * 0.6;
 
-
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowMetado < 0;
+        if (isSectionVisible) {
+          section.classList.add("ativo");
+        } else {
+          section.classList.remove("ativo");
+        }
+      });
+    }
+    animaScroll();
+    window.addEventListener("scroll", animaScroll);
+  }
+};
 
 initTabNav();
 initAccordionList();
 initScrollSuave();
-
+animaScroll();
