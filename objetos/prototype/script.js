ffd958321e84b510a568449de5c599af493d9624
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-04-19 22:45:59 -0300

    Prototype Constructor

--- objetos/prototype/script.js
+++ objetos/prototype/script.js
@@ -1,13 +1,42 @@
-function Pessoa(nome, idade) {
+// Crie uma função construtora de Pessoas
+// Deve conter nome, sobrenome e idade
+// Crie um método no protótipo que retorne
+// o nome completo da pessoa
+
+function Pessoa(nome, sobrenome, idade) {
   this.nome = nome;
+  this.sobrenome = sobrenome;
   this.idade = idade;
 }
 
-Pessoa.prototype.andar = function() {
-  return `${this.nome} andou pela rua`
+Pessoa.prototype.nomeCompleto = function() {
+  return `${this.nome} ${this.sobrenome}`;
 }
 
-const lucas = new Pessoa('Lucas', 23);
+const lucas = new Pessoa('Lucas', 'Gomes', 23);
 
-console.log(Pessoa.prototype)
-console.log(lucas.andar())
+
+// Liste os métodos acessados por 
+// dados criados com NodeList,
+// HTMLCollection, Document
+
+// console.log(Object.getOwnPropertyNames(NodeList.prototype));
+// console.log(Object.getOwnPropertyNames(HTMLCollection.prototype));
+// console.log(Object.getOwnPropertyNames(Document.prototype));
+
+// Liste os construtores dos dados abaixo
+const li = document.querySelector('li');
+
+li; // HTMLLIElement
+li.click; // Function
+li.innerText; // String
+li.value; // Number
+li.hidden; // Boolean
+li.offsetLeft; // Number
+li.click(); // undefined
+
+console.log(li.constructor.name)
+
+
+// Qual o construtor do dado abaixo:
+li.hidden.constructor.name; // String
