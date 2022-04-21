a6695a8b7908695874001f826fd1adefaede8286
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-04-20 23:51:39 -0300

    Native, Host e User

--- objetos/native-host-user/script-aula.js
+++ objetos/native-host-user/script-aula.js
@@ -1,111 +1,72 @@
-// PROTOTYPE
-// A propriedade prototype é um objeto adicionado a uma função quando a mesma é criada.
+// NATIVE
+// Objetos nativos são aqueles definidos na especificação da linguagem e são implementados independente do host.
 
-function Pessoa(nome, idade) {
-  this.nome = nome;
-  this.idade = idade;
-}
-const andre = new Pessoa('André', 28);
+// Construtores de objetos nativos
+Object
+String
+Array
+Function
 
-console.log(Pessoa.prototype); // retorna o objeto
-console.log(andre.prototype); // undefined
+// HOST
+// Objetos do host são aqueles implementados pelo próprio ambiente. Por exemplo no browser possuímos objetos do DOM, como DomList, HTMLCollection e outros. Em Node.js os objetos do Host são diferentes, já que não estamos em um ambiente do browser.
 
-
-// FUNCAO.PROTOTYPE
-// É possível adicionar novas propriedades e métodos ao objeto prototype.
-
-Pessoa.prototype.andar = function() {
-  return this.nome + ' andou';
-}
-Pessoa.prototype.nadar = function() {
-  return this.nome + ' nadou';
-}
-console.log(Pessoa.prototype); // retorna o objeto
+// Objetos do browser
+NodeList
+HTMLCollection
+Element
 
 
-// ACESSO AO PROTÓTIPO
-// O objeto criado utilizando o construtor, possui acesso aos métodos e propriedades do protótipo deste construtor. Lembrando, prototype é uma propriedade de funções apenas.
+// USER
+// Objetos do user, são os objetos definidos pelo seu aplicativo. Ou seja, qualquer objeto que você criar ou que importar de alguma biblioteca externa.
 
-const lucas = new Pessoa('André', 28);
-
-lucas.nome;
-lucas.idade;
-lucas.andar();
-lucas.nadar();
-
-// PROTO
-// O novo objeto acessa os métodos e propriedades do protótipo através da propriedade __proto__. É papel da engine fazer essa busca, não devemos falar com __proto__ diretamente.
-
-// Acessam o mesmo método
-// mas __proto__ não terá
-// acesso ao this.nome
-andre.andar();
-andre.__proto__.andar();
-
-// HERANÇA DE PROTÓTIPO
-
-// O objeto possui acesso aos métodos e propriedades do protótipo do construtor responsável por criar este objeto. O objeto abaixo possui acesso a métodos que nunca definimos, mas são herdados do protótipo de Object.
-
-Object.prototype;
-andre.toString();
-andre.isPrototypeOf();
-andre.valueOf();
-
-// CONSTRUTORES NATIVOS
-// Objetos, Funções, Números, Strings e outros tipos de dados são criados utilizando construtores. Esses construtores possuem um protótipo com propriedades e métodos, que poderão ser acessadas pelo tipo de dado.
-
-const pais = 'Brasil';
-const cidade = new String('Rio');
-
-pais.charAt(0); // B
-cidade.charAt(0); // R
-
-String.prototype;
-
-
-// É POSSÍVEL ACESSAR A FUNÇÃO DO PROTÓTIPO
-// É comum, principalmente em códigos mais antigos, o uso direto de funções do protótipo do construtor Array.
-
-const lista = document.querySelectorAll('li');
-
-// Transforma em uma array
-const listaArray = Array.prototype.slice.call(lista);
-
-// Existe o método Array.from()
-
-// MÉTODO DO OBJETO VS PROTÓTIPO
-// Nos objetos nativos existem métodos linkados diretamente ao Objeto e outros linkados ao protótipo.
-
-Array.prototype.slice.call(lista);
-Array.from(lista);
-
-// Retorna uma lista com os métodos / propriedades
-Object.getOwnPropertyNames(Array);
-Object.getOwnPropertyNames(Array.prototype);
-// dado.constructor.name, retorna o nome do construtor;
-
-[1,2,3].slice(); // existe
-[1,2,3].from(); // não existe
-
-
-// ENTENDA O QUE ESTÁ SENDO RETORNADO
-
-// Os métodos e propriedades acessado com o . são referentes ao tipo de dados que é retornado antes desse .
-
-const Carro = {
-  marca: 'Ford',
-  preco: 2000,
-  acelerar() {
-    return true;
-  }
+const Pessoa = {
+  nome: 'André'
 }
 
-Carro // Object
-Carro.marca // String
-Carro.preco // Number
-Carro.acelerar // Function
-Carro.acelerar() // Boolean
-Carro.marca.charAt // Function
-Carro.marca.charAt(0) // String
+// DIFERENTES VERSÕES
+// Browsers diferentes
 
-// Verifique o nome do construtor com .constructor.name
+// Apesar de tentarem ao máximo manter um padrão, browsers diferentes possuem objetos com propriedades e métodos diferentes.
+
+// Versões de browsers
+
+// Sempre que o browser é atualizado, novos objetos, métodos e propriedades podem ser implementados.
+
+// Host e Native Objects
+
+// Por exemplo, browsers que não implementaram o ECMAScript 2015 (ES6), não possuem o método find de Array.
+
+
+// VERSÕES DE JAVASCRIPT
+
+/**
+ECMA
+
+Organização responsável por definir padrões para tecnologias. ECMAScript é o padrão de JavaScript.
+
+ECMAScript 2015 ou ES6
+
+ES é uma abreviação de ECMAScript, ES6 é a sexta versão do ECMAScript, que foi lançada em 2015. Por isso ECMAScript 2015 é igual a ES6. A partir da ES6, existe uma tendência anual de atualizações. ECMAScript 2015, 2016, 2017, 2018 e Next.
+
+Engine
+
+Existem diversas engines que implementam o ECMAScript como V8, SpiderMonkey, Chakra, JavaScriptCore e mais.
+ */
+
+// BIBLIOTECAS
+
+//Bibliotecas como jQuery foram criadas para resolver o problema de inconsistências entre browsers e adicionar funcionalidades que não existiam nativamente. A padronização dos browsers e a implementação de soluções nativas, torna as mesmas obsoletas.
+
+$('a').addClass('ativo');
+$('a').hide();
+$('a').show();
+
+
+// VERIFICAR SE EXISTE
+//O typeof retorna o tipo de dado. Caso esse dado não exista ou não tenha sido definido, ele irá retornar undefined. Ou seja, quando não for undefined quer dizer que existe.
+
+if (typeof Array.from !== "undefined");
+if (typeof NodeList !== "undefined");
+
+// API
+// Application Programming Interface, é uma interface de software criada para a interação com outros softwares. Ou seja, toda interação que fazemos com o browser utilizando Objetos, Métodos e Propriedades, estamos na verdade interagindo com a API do browser.
