a9dd5fe26330a33eca176dc850acd948e2d7fcfd
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-05-11 12:16:09 -0300

    module

---
+++ efeitos-no-dom/modulos/assets/js/script.js
@@ -0,0 +1,11 @@
+import initTabNav from "./modules/tabnav.js";
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimacaoScroll from "./modules/scroll-animacao.js"
+import initAccordionList from "./modules/accordion-list.js"
+
+
+
+initTabNav();
+initScrollSuave();
+initAnimacaoScroll();
+initAccordionList();
