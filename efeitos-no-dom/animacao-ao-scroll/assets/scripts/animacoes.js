a4fbaac3180632a876ea623300b7339ee272acef
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-05-11 10:30:06 -0300

    dataset

---
+++ efeitos-no-dom/animacao-ao-scroll/assets/scripts/animacoes.js
@@ -0,0 +1,88 @@
+const initTabNav = () => {
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(".js-tabcontent section");
+
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add("ativo");
+    const activeTab = (index) => {
+      tabContent.forEach((item) => {
+        item.classList.remove("ativo");
+      });
+      tabContent[index].classList.add("ativo");
+    };
+
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener("click", () => activeTab(index));
+    });
+  }
+};
+
+const initAccordionList = () => {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+  const activeClass = "ativo";
+
+  if (accordionList.length) {
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.toggle(activeClass);
+
+    function activeAccordion() {
+      this.classList.toggle(activeClass);
+      this.nextElementSibling.classList.toggle(activeClass);
+    }
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  }
+};
+
+const initScrollSuave = () => {
+  const liksInternos = document.querySelectorAll('.js-menu a[href^="#"');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+
+    // Forma alternativa
+    // const topo = section.offsetTop
+    // window.scrollTo({
+    //   top: topo,
+    //   behavior: 'smooth'
+    // });
+  }
+
+  liksInternos.forEach((link) =>
+    link.addEventListener("click", scrollToSection)
+  );
+};
+
+const initScroll = () => {
+  const sections = document.querySelectorAll(".js-scroll");
+  if (sections.length) {
+    const windowMetado = window.innerHeight * 0.6;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = (sectionTop - windowMetado) < 0;
+        if (isSectionVisible) {
+          section.classList.add("ativo");
+        } else {
+          section.classList.remove("ativo");
+        }
+      });
+    }
+    animaScroll();
+    window.addEventListener("scroll", animaScroll);
+  }
+};
+
+initTabNav();
+initAccordionList();
+initScrollSuave();
+initScroll();
