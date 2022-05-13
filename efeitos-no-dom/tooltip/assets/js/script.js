c59c5263d3dd1a8868bae8819a040d17f34d0d33
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-05-13 10:39:10 -0300

    tooltip

---
+++ efeitos-no-dom/tooltip/assets/js/script.js
@@ -0,0 +1,14 @@
+import initTabNav from "./modules/tabnav.js";
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimacaoScroll from "./modules/scroll-animacao.js"
+import initAccordionList from "./modules/accordion-list.js"
+
+import initModal from "./modules/modal.js"
+
+
+
+initTabNav();
+initScrollSuave();
+initAnimacaoScroll();
+initAccordionList();
+initModal();
