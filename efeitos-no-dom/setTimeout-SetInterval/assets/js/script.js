61fc9ad37c162e1274c28954a65d42fd86ecb8f2
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-05-15 22:32:11 -0300

    setTimeout e setInterval

---
+++ efeitos-no-dom/setTimeout-SetInterval/assets/js/script.js
@@ -0,0 +1,18 @@
+import initTabNav from "./modules/tabnav.js";
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimacaoScroll from "./modules/scroll-animacao.js"
+import initAccordionList from "./modules/accordion-list.js"
+import initTooltip from "./modules/tooltip.js"
+
+import initModal from "./modules/modal.js"
+import initDropdownMenu from "./modules/dropdown-menu.js"
+
+
+
+initTabNav();
+initScrollSuave();
+initAnimacaoScroll();
+initAccordionList();
+initModal();
+initTooltip();
+initDropdownMenu();
