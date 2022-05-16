7b07a604a2755cc3facca53fb6a0d435da1236aa
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-05-16 10:15:22 -0300

    setTimeout e setInterval

--- efeitos-no-dom/setTimeout-SetInterval/assets/js/script-aula.js
+++ efeitos-no-dom/setTimeout-SetInterval/assets/js/script-aula.js
@@ -1,74 +1,92 @@
-// Dataset
+// setTimeout e setInterval
 
+// SETTIMEOUT()
 
-// HTMLELEMENT
-// Todo elemento HTML do DOM herda propriedades e métodos do construtor HTMLElement.
+// setTimeout(callback, tempo, arg1, arg2, ...) método assíncrono que ativa o callback após tempo. Não existe garantia de que o código será executado exatamente após o tempo, pois o callback entra na fila e espera pela Call Stack estar vazia.
 
-const h1 = document.querySelector('h1');
-Object.prototype.toString.call(h1); // [object HTMLHeadingElement]
-// HTMLHeadingElement > HTMLElement > Element > Node > EventTarget > Object
+function espera(texto) {
+  console.log(texto);
+}
+setTimeout(espera, 1000, 'Depois de 1s');
 
+// IMEDIATO
+// Se não passarmos o argumento de tempo ele irá assumir o valor 0 e entrará na fila imediatamente para ser executado. Podemos passar uma função anônima diretamente com argumento.
 
-// DATASET
-// dataset é uma propriedade de HTMLElement, essa propriedade é um objeto do tipo DOMStringMap. Dentro desse objeto existe uma coleção de chave / valor, com todos os atributos do elemento html que começarem com data-.
-
-`<div data-cor="azul" data-width="500">Uma Div</div>
-<span data-anime="left" data-tempo="2000">Um Span</span> 
-
-// Ambos os valores selecionam a mesma div acima.
-let div = document.querySelector('div');
-div = document.querySelector('[data-cor]');
-div = document.querySelector('[data-cor="azul"]');
-
-div.dataset;
-// DOMStringMap {cor: "azul", width: "500"}
-div.dataset.cor; // 'azul'
-div.dataset.width; // '500'
-div.dataset.tempo = 1000;
-// DOMStringMap {cor: "azul", width: "500", tempo: "1000"}
-
-`
-
-// DATA ATRIBUTES
-// Os atributos e valores que começarem com data- poderão ser utilizados como forma de configuração de plugins e interações DOM / JS.
-
-`
-<div data-anima="left" data-tempo="1000">Div 1</div>
-<div data-anima="right" data-tempo="2000">Div 2</div>
-
-`
-
-const divs = document.querySelectorAll('[data-anima]');
-divs.forEach((div) => {
-  div.classList.add(div.dataset.anima);
+setTimeout(() => {
+  console.log('Após 0s?');
 });
 
-// adiciona em cada div
-// uma classe com o mesmo nome
-// que o valor de data
+
+// LOOPS E SETTIMEOUT
+// Um loop é executado rapidamente, em milissegundos. Se colocarmos um setTimeout dentro do loop, todos eles serão adicionados à Web Api praticamente no mesmo tempo. Um evento de setTimeout não espera o tempo do anterior acabar para iniciar.
+
+for(let i = 0; i < 20; i++) {
+  setTimeout(() => {
+    console.log(i);
+  }, 300);
+}
+
+// CORRIGINDO O LOOP
+// Agora ele está multiplicando o tempo por i. Assim o primeiro aparecerá em 0ms, o segundo em 300ms, o terceiro em 600ms e assim em diante.
+
+for(let i = 0; i < 20; i++) {
+  setTimeout(() => {
+    console.log(i);
+  }, 300 * i);
+}
 
 
-// DATA VS CLASS
-// A vantagem de se utilizar data atributes é que torna mais fácil evitarmos conflitos com estilos do CSS. Além de deixar a estrutura da tag mais organizada.
+// THIS E WINDOW
+// setTimeout é um método do objeto Window. O valor de this dentro do mesmo callback é uma referência ao seu objeto no caso Window.
 
-`
-<div data-anima="left" data-tempo="1000">Div 1</div>
-<div class="anima-left tempo-1000">Div 2</div>
+const btn = document.querySelector('button');
+btn.addEventListener('click', handleClick);
 
-`
+function handleClick(event) {
+  setTimeout(function() {
+    this.classList.add('active');
+  }, 1000)
+}
+// Erro pois window.classList não existe
 
-// NOMENCLATURA
-// Por padrão o JavaScript não aceita - (traço) como caracter válido para nomear propriedades. Então qualquer traço será removido e a letra seguinte transformada em maiúscula.
+// ARROW FUNCTION
+// Quando utilizamos uma Arrow Function como callback, o contexto de this passa a ser do local onde o setTimeout foi iniciado.
 
-`
-<div data-anima-scroll="left">Div 1</div>
-`
+const btn2 = document.querySelector('button');
+btn2.addEventListener('click', handleClick);
 
-const div = document.querySelector('[data-anima-scroll]');
-div.dataset;
-// {animaScroll: 'left'}
-div.dataset.animaScroll; // left
-div.dataset.tempoTotal = 1000;
-// Na div vira data-tempo-total="1000"
+// this agora é btn.
+function handleClick(event) {
+  setTimeout(() => {
+    this.classList.add('active');
+  }, 1000)
+}
 
-delete div.dataset.animaScroll; // remove o atributo
+
+// SETINTERVAL
+// setInterval(callback, tempo, arg1, arg2, ...), irá ativar o callback toda vez que a quantidade de tempo passar.
+
+function loop(texto) {
+  console.log(texto);
+}
+setInterval(loop, 1000, 'Passou 1s');
+
+// loop a cada segundo
+let i = 0;
+setInterval(() => {
+  console.log(i++);
+}, 1000);
+
+
+// CLEARINTERVAL
+// clearInterval(var), podemos parar um intervalo com o clearInterval. Para isso precisamos atribuir o setInterval a uma variável.
+
+const contarAte10 = setInterval(callback, 1000);
+
+let i = 0;
+function callback() {
+  console.log(i++);
+  if (i > 10) {
+    clearInterval(contarAte10);
+  }
+}
