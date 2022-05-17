ca72883599956050762fa9123daa20165f09ff3d
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-05-17 11:00:18 -0300

    anima numeros

---
+++ efeitos-no-dom/anima-numeros/assets/js/script.js
@@ -0,0 +1,22 @@
+import initTabNav from "./modules/tabnav.js";
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimacaoScroll from "./modules/scroll-animacao.js"
+import initAccordionList from "./modules/accordion-list.js"
+import initTooltip from "./modules/tooltip.js"
+
+import initModal from "./modules/modal.js"
+import initDropdownMenu from "./modules/dropdown-menu.js"
+
+import initMenuMobile from "./modules/menu-mobile.js"
+import initAnimaNumeros from "./modules/anima-numeros.js"
+
+
+initTabNav();
+initScrollSuave();
+initAnimacaoScroll();
+initAccordionList();
+initModal();
+initTooltip();
+initDropdownMenu();
+initMenuMobile();
+initAnimaNumeros();
