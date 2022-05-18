b9af2ac5f8b5ea82ba98e4bd5af9717f0d5caa9b
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-05-18 09:10:48 -0300

    data object

---
+++ efeitos-no-dom/data-object/assets/js/script.js
@@ -0,0 +1,24 @@
+import initTabNav from "./modules/tabnav.js";
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimacaoScroll from "./modules/scroll-animacao.js"
+import initAccordionList from "./modules/accordion-list.js"
+import initTooltip from "./modules/tooltip.js"
+
+import initModal from "./modules/modal.js"
+import initDropdownMenu from "./modules/dropdown-menu.js"
+
+import initMenuMobile from "./modules/menu-mobile.js"
+import initAnimaNumeros from "./modules/anima-numeros.js"
+import initFuncionamento from "./modules/funcionamento.js"
+
+
+initTabNav();
+initScrollSuave();
+initAnimacaoScroll();
+initAccordionList();
+initModal();
+initTooltip();
+initDropdownMenu();
+initMenuMobile();
+initAnimaNumeros();
+initFuncionamento();
