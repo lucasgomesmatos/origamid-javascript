3ab87a0ad1b7a913ae69630e58825fc7e923810c
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-06-05 20:45:09 -0300

    Regexp seleção

---
+++ regular-expression/regexp-selecao/script-aula.js
@@ -0,0 +1,354 @@
+// https://regexr.com/
+
+// REGULAR EXPRESSION
+// Regexp ou Regex são expressões utilizadas para realizarmos buscas / substituições de padrões em strings. Os padrões devem ser colocados entre //. Geralmente vamos utilizá-las nos métodos .replace() e .split().
+
+// Procura: a
+const padraoRegexp = /j/gi;
+
+const texto = 'JavaScript';
+const novoTexto = texto.replace(padraoRegexp, 'B');
+// BavaScript
+
+console.log(novoTexto);
+
+// LITERAL
+// Utilizar um caracter literal irá realizar uma busca específica deste caracter.
+
+// Procura: J seguido de a, v e a
+const regexpJava = /Java/;
+
+'JavaScript'.replace(regexpJava, 'Type');
+// TypeScript
+
+
+// FLAG: G
+// As flags irão modificar como a expressão é interpretada. Uma das mais utilizadas é a g, que significa global, ou seja, retorne todos os resultados que estiverem dentro do padrão e não apenas o primeiro. A flag deve ser colocada no final da expressão.
+
+// Procura: Todo a
+const regexpG = /a/g;
+
+'JavaScript'.replace(regexpG, 'i');
+// JiviScript
+
+// FLAG: I
+// Com o i informamos que devem ser ignoradas as diferenças entre maiúsculas e minúsculas. Isso significa que /a/ irá buscar por a e A.
+
+// Procura: Todo PE, Pe, pE e pe
+const regexpI = /Pe/gi;
+
+'Perdeu perdido'.replace(regexpI, 'Ba');
+// Bardeu Bardido
+
+// CHARACTER CLASS
+// Se colocarmos os caracteres entre colchetes, estamos definindo uma classe. /[ab]/ irá procurar por a ou por b.
+
+// Procura: Todo a, A, i, I
+const regexpCLASS = /[ai]/gi;
+
+'JavaScript'.replace(regexpCLASS, 'u');
+// JuvuScrupt
+
+
+// CHARACTER CLASS E ESPECIAIS
+// Podemos utilizar caracteres que não são alfanuméricos dentro da classe. Mas fique atento, pois existem diversos casos especiais para os mesmos.
+
+// Procura: - ou .
+const regexpCLASS_ES = /[-.]/g;
+
+'111.222-333-44'.replace(regexpCLASS_ES, '');
+// 11122233344
+
+
+// UM OU OUTRO
+// Combine caracteres literais com uma classe para buscarmos variações: Ju[nl]ho busca Julho ou Junho.
+
+// Procura: B, seguido de r, a
+// seguido de s ou z, seguido de i, l
+const regexp = /Bra[sz]il/g;
+
+'Brasil é com z: Brazil'.replace(regexp, 'Prazer');
+// Prazer é com z: Prazer
+
+
+// DE A À Z
+// O traço - dentro de [] pode servir para definirmos um alcance. [A-Z] irá buscar os caracteres de A à Z. [0-9] busca de 0 à 9. A tabela UNICODE é utilizada como referência para definir os caracteres dentro do alcance.
+
+// Busca por itens de a à z
+const regexpAZ = /[a-z]/g;
+
+'JavaScript é a linguagem.'.replace(regexpAZ, '0');
+// J000S00000 é 0 000000000.
+
+// Busca por itens de a à z e A à Z
+const regexpAZM = /[a-zA-Z]/g;
+
+'JavaScript é a linguagem.'.replace(regexpAZM, '1');
+// 1111111111 é 1 111111111.
+
+// Busca por números de 0 à 9
+const regexpNumero = /[0-9]/g;
+
+'123.333.333-33'.replace(regexpNumero, 'X');
+// XXX.XXX.XXX-XX
+
+// NEGAR
+// Utilizando o acento circunflexo podemos negar caracteres. Ou seja, pegue tudo que não seja [^a]
+
+// Procura: tudo que não estiver entre a e z
+const regexpNegar = /[^a-z]/g;
+
+'Brasil é com z: Brazil'.replace(regexpNegar, ' ');
+// rasil   com z   razil 
+
+// PONTO
+// O ponto . irá selecionar qualquer caracter, menos quebras de linha.
+
+// Procura: todos os caracteres menos quebra de linha
+const regexpPonto = /./g;
+
+'JavaScript é a linguagem.'.replace(regexpPonto, '0');
+// 0000000000000000000000000
+
+
+// Escapar Especiais
+// Caracteres especiais como o ponto ., podem ser escapados utilizando a barra \, assim este não terá mais a sua função especial e será tratado como literal. Lista de caracteres especiais: +*?^$\.[]{}()|/
+
+// Procura: todos os pontos
+const regexpES = /\./g;
+const regexpAlternativa = /[.]/g;
+
+'999.222.222.11'.replace(regexpES, '-');
+// 999-222-222-11
+
+
+// Word
+// O \w irá selecionar qualquer caracter alfanumérico e o underline. É a mesma coisa que [A-Za-z0-9_].
+
+// Procura: todos os alfanuméricos
+const regexpWord = /\w/g;
+
+'Guarda-chuva R$ 23,00.'.replace(regexpWord, '-');
+// ------------ -$ --,--.
+
+
+// Not Word
+// O \W irá selecionar tudo o que não for caracter alfanumérico e o underline. É a mesma coisa que [^A-Za-z0-9_].
+
+
+// Procura: o que não for caracter alfanuméricos
+const regexpNotWord = /\W/g;
+
+'Guarda-chuva R$ 23,00.'.replace(regexpNotWord, '-');
+// Guarda-chuva-R--23-00-
+
+// Digit
+// O \d irá selecionar qualquer dígito. É a mesma coisa que [0-9].
+
+// Procura: todos os dígitos
+const regexpD = /\d/g;
+
+'+55 (21) 2222-2222'.replace(regexpD, 'X');
+// +XX (XX) XXXX-XXXX.
+
+// Not Digit
+// O \D irá selecionar tudo que não for dígito. É a mesma coisa que [^0-9].
+
+// Procura: o que não for dígito
+const regexpNotD = /\D/g;
+
+'+55 (21) 2222-2222'.replace(regexpNotD, '');
+// 552122222222
+
+
+// Whitespace
+// O \s irá selecionar qualquer espaço em branco, isso inclui espaços, tabs, quebra de linhas.
+
+// Procura: espaços em branco
+const regexpSpace = /\s/g;
+
+'+55 (21) 2222-  2222  '.replace(regexpSpace, '');
+// +55(21)2222-2222
+
+// Not Whitespace
+// O \S irá selecionar qualquer caracter que não for espaço em branco.
+
+// Procura: o que não for espaço em branco
+const regexpNSpace = /\S/g;
+
+'+55 (21) 2222-  2222  '.replace(regexpNSpace, '');
+// XXX XXXX XXXXX  XXXX  
+
+// /[\s\S]/g irá selecionar tudo.
+
+
+// Quantificador
+// É possível selecionar caracteres seguidos, como /bbb/g irá selecionar apenas bbb. Com as chaves podemos indicar a repetição /b{3}/g. Agora ele está fazendo uma seleção completa e não caracter por caracter.
+
+// Procura: 4 a's seguidos
+const regexpQ = /aaaa/g;
+const regexpAlt = /a{4}/g;
+
+'Vaaaai ali por favor.'.replace(regexpQ, 'a');
+// Vai ali por favor.  
+
+
+// Quantificador Min e Max
+// Podemos informar o min e max do quantificador /a{2,4}/ vai selecionar quando aparecer a duas vezes ou até 4 vezes. /a{2,}/ irá selecionar quando se repetir duas ou mais vezes.
+
+// Procura: dígitos seguidos de 2 à 3
+const regexpQMM = /\d{2,3}/g;
+
+'222.333.222.42'.replace(regexpQMM, 'X');
+// X.X.X.X
+
+// Procura: letras seguidos com 1 caracter ou mais
+const regexpLetras = /\w{1,}/g;
+
+'A melhor linguagem é JavaScript'.replace(regexpLetras, 'X');
+// X X X é X
+
+// Mais +
+// O sinal de + significa que devemos selecionar quando existir pelo menos uma ou mais ocorrências.
+
+// Procura: dígitos em ocorrência de um ou mais
+const regexpM = /\d+/g;
+
+'222.333.222.42'.replace(regexpM, 'X');
+// X.X.X.X
+
+// Procura: Começa com d, seguido por uma ou mais letras.
+const regexpLetrasM = /d\w+/g;
+
+'Dígitos, dados, desenhos, Dito, d'.replace(regexpLetrasM, 'X');
+// Dígitos, X, X, Dito, d
+
+
+// Dígitos, X, X, Dito, d
+// O sinal * significa que devemos selecionar quando existir 0 ou mais ocorrências.
+
+// Procura: Começa com d, seguido por zero ou mais letras.
+const regexpAs = /d\w*/g;
+
+'Dígitos, dados, desenhos, Dito, d'.replace(regexpAs, 'X');
+// Dígitos, X, X, Dito, X
+
+
+// Opcional ?
+// O sinal ? significa que o caracter é opcional, pode ou não existir.
+
+// Procura: Por regex com p opcional
+const regexpOP = /regexp?/g;
+
+'Qual é o certo, regexp ou regex?'.replace(regexpOP, 'Regular Expression');
+// Qual é o certo, Regular Expression ou Regular Expression?
+
+// Alternado |
+// O sinal | irá selecionar um ou outro. java|php
+
+// Procura: java ou php (case insensitive)
+const regexpALT = /java|php/gi;
+
+'PHP e Java são linguagens diferentes'.replace(regexpALT, 'X');
+// X e X são linguagens diferente
+
+
+// Word Boundary \b
+// O sinal \b irá indicar que pretendemos fazer uma seleção que deve ter início e fim de não caracteres \w.
+
+// Procura: java (case insensitive)
+const regexpWB = /java/gi;
+'Java não é JavaScript.'.replace(regexpWB, 'X');
+// X não é XScript.
+
+// Procura: java (case insensitive)
+const regexpBoundary = /\bjava\b/gi;
+'Java não é JavaScript.'.replace(regexpBoundary, 'X');
+// X não é JavaScript.
+
+// Procura: Dígitos em sequência, que estejam isolados
+const regexpDigito = /\b\d+\b/gi;
+'O Restaurante25 na Rua 3, custa R$ 32,00'.replace(regexDigito, 'X');
+// O Restaurante25 na Rua X, custa R$ X,X
+
+'11_22 33-44 55é66 77e88'.replace(regexpDigito, 'X');
+// 11_22 X-X XéX 77e88
+
+// Not Word Boundary \B
+// É o contrário do \b
+
+const regexpDigitoNW = /\B\d+\B/gi;
+
+'11_22 33-44 55é66 77e88'.replace(regexpDigitoNW, 'X');
+// 1X_X2 33-44 55é66 7XeX8
+
+
+// Anchor Beginning
+// Com o ^ é possível informar que a busca deve ser iniciada no início da linha.
+
+// Procura: sequência de alfanuméricos
+// no início da linha.
+const regexpAB = /^\w+/g;
+
+`andre@origamid.com
+contato@origamid.com`.replace(regexpAB, 'X');
+// X@origamid.com
+// contato@origamid.com
+
+
+
+// Anchor End
+// Com o $ é possível informar que a busca deve ser iniciada no final da linha.
+
+// Procura: sequência de alfanuméricos
+// no final da linha.
+const regexpAEND = /\w+$/g;
+
+`andre@origamid.com
+contato@origamid.com`.replace(regexpAEND, 'X');
+// andre@origamid.com
+// contato@origamid.X
+
+
+// Flag: m
+// Com a flag m de multiline, podemos informar que a busca de início ^ e final $ de linha devem ocorrer em todas as linhas disponíveis
+
+// Procura: sequência de alfanuméricos
+// no final da linha.
+const regexpFM = /\w+$/gm;
+
+`andre@origamid.com
+contato@origamid.com`.replace(regexpFM, 'X');
+// andre@origamid.X
+// contato@origamid.X
+
+// Procura: sequência de alfanuméricos
+// no início da linha.
+const regexpFMS = /^\w+/gm;
+
+`andre@origamid.com
+contato@origamid.com`.replace(regexpFMS, 'X');
+// X@origamid.com
+// X@origamid.com
+
+// Line Feed \n
+// O \n irá selecionar o final de uma linha, quando criamos uma nova.
+
+const regexpLF = /\n/g;
+
+`andre@origamid.com\ncontato@origamid.com`.replace(regexpLF, '---');
+// andre@origamid.com---contato@origamid.com
+
+`andre@origamid.com
+contato@origamid.com`.replace(regexpLF, 'X');
+// andre@origamid.com---contato@origamid.com
+
+
+// Unicode \u
+// O \u irá selecionar o respectivo caracter unicode, de acordo com o código passado em \uXXXX. Ex: \u0040 seleciona o @.
+
+// Procura: @ ou ©
+const regexpUnicode = /\u0040|\u00A9/g;
+
+'andre@origamid.com ©'.replace(regexpUnicode, '---');
+// andre---origamid.com ---
--- regular-expression/regexp-selecao/script.js
+++
@@ -1,5 +0,0 @@
-const frase = 'JavaScript';
-const regexp = /[a-z]/gi
-const fraseNova = frase.replace(regexp, "B");
-
-console.log(fraseNova);
