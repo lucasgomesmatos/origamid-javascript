c16be4928e8892ea12e95740922ac0337b84a8de
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-06-08 09:42:14 -0300

    regexp metodo function

--- regular-expression/regexp-metodos/script.js
+++ regular-expression/regexp-metodos/script.js
@@ -14,11 +14,34 @@
 //   console.log(i++, regexp.lastIndex);
 // }
 
-const frase = 'JavaScript, TypeScript, CoffeeScript, Java';
-const regexp = /Script/g;
+// const frase = 'JavaScript, TypeScript, CoffeeScript, Java';
+// const regexp = /Script/g;
 
-let regexpResultado;
+// let regexpResultado;
 
-while((regexpResultado = regexp.exec(frase)) !== null){
-  console.log(regexpResultado);
-}
+// while((regexpResultado = regexp.exec(frase)) !== null){
+//   console.log(regexpResultado);
+// }
+
+const emails = `
+joao@homail.com
+marta@ggmail.com
+bruna@oulook.com
+lucas@likearocket.com`;
+
+
+const regexp = /(\w+@)([\w.]+)/g;
+
+const resultado = emails.replace(regexp, (...args) => {
+  if(args[2] === 'homail.com') {
+    return `${args[1]}hotmail.com`;
+  } else if (args[2] === 'ggmail.com') {
+    return `${args[1]}gmail.com`;
+  } else if (args[2] === 'oulook.com') {
+    return `${args[1]}outlook.com`;
+  } else { 
+    return args[0];
+  }
+});
+
+console.log(resultado);
